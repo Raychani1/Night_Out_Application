8b4f4cf5d1f239d260989901c8da1f2bd397cc18
Raychani1 <rajcsanyi.ladislav.it@gmail.com>
2022-12-09 11:18:49 +0100

    Add Models

---
+++ app/src/main/java/com/example/android/i_mobv_application/model/Friend.kt
@@ -0,0 +1,8 @@
+package com.example.android.i_mobv_application.model
+
+data class Friend(
+	val userId: String,
+	val username: String,
+	val currentPubId: String?,
+	val currentPubName: String?,
+)
